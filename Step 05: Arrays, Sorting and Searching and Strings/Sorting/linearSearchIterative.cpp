66844bba710bb863f1be4aa0a3d8664e007a1b61
garrymarkus <premsinghiitjee@gmail.com>
2026-01-21 11:10:15 +0530

    cleaning data

--- Step 05: Arrays, Sorting and Searching and Strings/Sorting/linearSearchIterative.cpp
+++
@@ -1,7 +0,0 @@
-int linearSearchIterative(const vector<int>& v, int key) {
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] == key)
-            return i;
-    }
-    return -1;
-}
--- Step 05: Arrays, Sorting and Searching and Strings/Sorting/lsRecursive.cpp
+++
@@ -1,7 +0,0 @@
-int linearSearchRecursive(const vector<int>& v, int key, int i) {
-    if (i == v.size())
-        return -1;
-    if (v[i] == key)
-        return i;
-    return linearSearchRecursive(v, key, i + 1);
-}
