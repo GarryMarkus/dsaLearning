81c0418d8f4ae032d10d94dd7e9ea25337b0fbdb
GarryMarkus <premsinghiitjee@gmail.com>
2026-07-21 06:24:25 +0530

    binary search files updated

--- Lecture_12/Codes/binary_search.cpp
+++ Lecture_12/Codes/binary_search.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int binarySearch(int arr[], int size, int key) {
@@ -18,20 +18,20 @@ int binarySearch(int arr[], int size, int key) {
         if (key > arr[mid]) {
             start = mid + 1;
         }
-        
+
         //key < arr[mid] go to left wala part
-        else { 
+        else {
             end = mid - 1;
         }
 
         mid = start + (end-start)/2;
     }
-    
+
     return -1;
 }
 
 
-int main() { 
+int main() {
 
     int even[6] = {2,4,6,8,12,18};
     int odd[5] = {3, 8, 11, 14, 16};
